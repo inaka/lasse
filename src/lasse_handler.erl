74592a563f33b3c43859bc5e95ba764817b5e07f
Juan Facorro <juan.facorro@gmail.com>
2014-06-06 18:02:45 -0300

    [#1] Improved implementation. Added and renamed behavior functions. Removed event record.

--- src/lasse_handler.erl
+++ src/lasse_handler.erl
@@ -4,48 +4,66 @@
 -export([
          init/3,
          info/3,
+         notify/2,
          terminate/3
         ]).
 
--record(event, {id, name, data, retry}).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Records
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-record(state, {module, state}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Behavior definition
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--type lasse_sse_message() :: tuple(Event :: string(), Data :: binary() | [binary()]).
--type lasse_sse_error() :: tuple('error', Reason :: string()).
+-type event_value() :: {'id', binary()} | {'event', binary()} | {'data', binary()} | {'retry', binary()}.
+-type event() :: [event_value(), ...].
 
--callback init() -> 'ok' | lasse_sse_error().
--callback handle(Msg :: any()) -> tuple('ok', 'terminate' | lasse_sse_message()) | lasse_sse_error().
+-callback init(InitArgs :: any()) -> State :: any().
+-callback handle_notify(Msg :: any(), State :: any()) ->
+    tuple('send', Event :: event(), NewState :: any())
+        | tuple('nosend', NewState :: any())
+        | tuple('stop', NewState :: any()).
+-callback handle_info(Msg :: any(), Req :: any(), State :: any()) ->
+    tuple('send', Event :: event(), NewState :: any())
+        | tuple('nosend', NewState :: any())
+        | tuple('stop', NewState :: any()).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Cowboy callbacks
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--type lasse_handler_option()  :: {'module', module()}.
--type lasse_handler_options() :: [module(), ...] | [lasse_handler_option(), ...].
--type lasse_handler_state() :: [{'module', module()}].
+-type lasse_handler_option()  :: {'module', module()} | {'init_args', any()}.
+-type lasse_handler_options() :: [module()] | [lasse_handler_option(), ...].
 
--spec init(any(), any(), lasse_handler_options()) -> {loop, any(), lasse_handler_state()}.
+-spec init(any(), any(), lasse_handler_options()) -> {loop, any(), record(state)}.
 init(_Transport, Req, Opts) ->
-    Module = module_option(Opts),
-    Module:init(), % Should this accept an argument?
+    Module = get_value(module, Opts, hd(Opts)),
+    InitArgs = get_value(init_args, Opts, []),
+
+    State = Module:init(InitArgs),
 
     Headers = [{<<"content-type">>, <<"text/event-stream">>},
                {<<"cache-control">>, <<"no-cache">>}], % recommended to prevent caching of event data.
     {ok, Req2} = cowboy_req:chunked_reply(200, Headers, Req),
-    {loop, Req2, [{module, Module}]}.
 
-info({message, Msg}, Req, State = [{module, Module}]) ->
-    case Module:handle(Msg) of
-        {ok, Event} when is_record(Event, event) ->
-            EventMsg = build_event(Event),
-            ok = cowboy_req:chunk(EventMsg, Req),
-            {loop, Req, State};            
-        {ok, terminate} ->
-            {ok, Req, State}
-    end.
+    {loop, Req2, #state{module = Module, state = State}}.
+
+info({message, Msg}, Req, State) ->
+    Module = State#state.module,
+    ModuleState = State#state.state,
+    Result = Module:handle_notify(Msg, ModuleState),
+    process_result(Result, Req, State);
+info(Msg, Req, State) ->
+    Module = State#state.module,
+    ModuleState = State#state.state,
+    Result = Module:handle_info(Msg, ModuleState),
+    process_result(Result, Req, State).
+
+notify(Pid, Msg) ->
+    Pid ! {message, Msg}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
@@ -54,37 +72,53 @@ terminate(_Reason, _Req, _State) ->
 %% Helper functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--spec module_option(lasse_handler_options()) -> module().
-module_option(Opts) ->
-    case lists:keyfind(module, 1, Opts) of
-        {module, Module} -> Module;
-        _                   -> [Module] = Opts,
-                               Module
+process_result(Result, Req, State) ->
+    case Result of
+        {send, Event, NewState}  ->
+            EventMsg = build_event(Event),
+            ok = cowboy_req:chunk(EventMsg, Req),
+            {loop, Req, State#state{state = NewState}};
+        {nosend, NewState} ->
+            {loop, Req, State#state{state = NewState}};
+        {stop, NewState} ->
+            {ok, Req, State#state{state = NewState}}
     end.
 
+get_value(Key, PropList) ->
+    case lists:keyfind(Key, 1, PropList) of
+        {Key, Value} -> Value;
+        _ -> false
+    end.
 
-build_event(#event{id=Id, name=Name, data=Data, retry=Retry}) ->
-    [build_field("id: ", Id),
-     build_field("event: ", Name),
-     build_data(Data),
-     build_field("retry: ", Retry)].
+get_value(Key, PropList, NotFound) ->
+    case get_value(Key, PropList) of
+        false -> NotFound;
+        Value -> Value
+    end.
 
+build_event(Event) ->
+    [build_field(<<"id: ">>, get_value(id, Event)),
+     build_field(<<"event: ">>, get_value(name, Event)),
+     build_data(get_value(data, Event)),
+     build_field(<<"retry: ">>, get_value(retry, Event)),
+     <<"\n">>].
 
-build_field(_, undefined) ->
+build_field(_, false) ->
     [];
 build_field(_, "") ->
     [];
 build_field(Name, Value) ->
-    [Name, Value].    
+    [Name, Value, <<"\n">>].
 
-
+build_data(false) ->
+    throw(data_required);
 build_data(Data) ->
     build_data(Data, []).
 
 build_data([], Result) ->
     lists:reverse(Result);
 build_data([Data|MoreData], Result) ->
-    build_data(MoreData, [["\ndata:", Data] | Result]);
+    build_data(MoreData, [[<<"data: ">>, Data, <<"\n">>] | Result]);
 build_data(Data, Result) when is_binary(Data) ->
     SplitData = binary:split(Data, <<"\n">>, [global]),
     build_data(SplitData, Result).
