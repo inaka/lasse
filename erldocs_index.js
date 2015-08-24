001d41c8a47383e05b3edbf14e0453c93cf20469
Juan Facorro <juan.facorro@gmail.com>
2015-08-24 16:24:33 -0300

    [#18] GitHub page

---
+++ erldocs_index.js
@@ -0,0 +1,1 @@
+var index = [['app','lasse','lasse','[application]'],['mod','lasse','lasse_handler','Server-Sent Event handler for Cowboy.'],['fun','lasse','lasse_handler:info/1',[]],['fun','lasse','lasse_handler:init/1',[]],['fun','lasse','lasse_handler:notify/1',[]],['fun','lasse','lasse_handler:terminate/1',[]]];
